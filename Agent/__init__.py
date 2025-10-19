8e9fa892c1b6fd3ce33cd347cfc5519542c3fbe6
mxl <mxlscut153@163.com>
2025-10-19 20:17:26 +0800

    try to make into package

---
+++ Agent/__init__.py
@@ -0,0 +1,4 @@
+from .Core.agent_core import AgentCore
+
+__all__ = ["AgentCore"]
+__version__ = "0.1.0"
--- Agent/request/api.py
+++ Agent/request/api.py
@@ -22,10 +22,16 @@ def delete_conversation(*args, **kwargs):
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+ds_api_key = config["ds_api_key"] if "ds_api_key" in config else ""
+ds_base_url = config["ds_base_url"] if "ds_base_url" in config else ""
+
+doubao_api_key = config["doubao_api_key"] if "doubao_api_key" in config else ""
+doubao_base_url = config["doubao_base_url"] if "doubao_base_url" in config else ""
+
 
 client = OpenAI(
-    api_key=config["ds_api_key"],
-    base_url=config["ds_base_url"]
+    api_key=ds_api_key,
+    base_url=ds_base_url
 )
 
 
@@ -52,8 +58,8 @@ def get_response_from_Doubao(input, Memory: MemoryContainer):
     
     Memory._add_user_message(input)
     client = Ark(
-        api_key=config["doubao_api_key"],
-        base_url=config["doubao_base_url"]
+        api_key=doubao_api_key,
+        base_url=doubao_base_url
     )
     completion = client.chat.completions.create(
         model="doubao-seed-1-6-vision-250815",
