96992873719db4d1e8e708d4c9c582aa6e4d97b5
mxl <mxlscut153@163.com>
2025-10-19 19:43:54 +0800

    deleted Modules

--- Example.py
+++ Example.py
@@ -1,6 +1,6 @@
-from Agent.agent_core import AgentCore
+from Agent.Core.agent_core import AgentCore
 agent = AgentCore(UseModel="Doubao")
 agent.set_task("""
-读取docs/中的test.docx文档，帮我总结内容
+从网络搜索FDU的信息并帮我写入到docs文件夹里面
                """)
 agent.run()
